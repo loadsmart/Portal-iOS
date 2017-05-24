d290854e0769d8083dd4572f09ea1efee31cff53
Gustavo Barbosa <gustavocsb@gmail.com>
2017-05-24 17:48:57 -0400

    Initial commit

---
+++ Example/Portal/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Portal
+//
+//  Created by Gustavo Barbosa on 05/24/2017.
+//  Copyright (c) 2017 Gustavo Barbosa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
